59ea460d3058f6427cc9032ff14da40b97056f92
Giltroy P. Meren <giltroy.meren@GFI00245.local>
2022-01-22 14:04:03 +0800

    Removed useVideos hook deps

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,9 @@ import SearchBar from './components/SearchBar'
 import VideoDetail from './components/VideoDetail'
 import { IYouTubeResultItem } from './common/types'
 import VideoList from './components/VideoList';
-import useVideos from './hooks/useVideos'
 
 function App() {
   const [selectedVideo, setSelectedVideo] = useState<IYouTubeResultItem>()
-  const { list, onSearch } = useVideos('mochi')
-
-  useEffect(() => {
-    setSelectedVideo(list.data.items[0])
-  }, [list])
 
   const onSetSelectedVideo = (video: IYouTubeResultItem): void => {
     setSelectedVideo(video)
